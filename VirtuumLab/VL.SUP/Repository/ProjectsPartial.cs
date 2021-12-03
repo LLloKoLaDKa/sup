136916a2c4bb0a46d00c20ecb3734c72d3967b6a
v.ivanov <v.ivanov@virtuumlab.ru>
2021-12-03 07:26:18 +0300

    Переход с окон на страницы, изменение схемы

---
+++ VirtuumLab/VL.SUP/Repository/ProjectsPartial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace VLSUP.Repository
+{
+    public partial class Project
+    {
+        private readonly Guid BacklogTaskState = Guid.Parse("a95103de-0215-45ba-abf4-d76754d388c1");
+        private readonly Guid InWorkTaskState = Guid.Parse("53b8db15-40e2-4bfa-aecd-ebd7029dd0fc");
+        private readonly Guid CompletedTaskState = Guid.Parse("6a81d05e-ee85-4446-895a-b07019dcf0b3");
+
+        public Int32 BacklogTaskCount
+        {
+            get
+            {
+                return Tasks.Where(t => t.State == BacklogTaskState).Count();
+            }
+        }
+
+        public Int32 InWorkTaskCount
+        {
+            get
+            {
+                return Tasks.Where(t => t.State == InWorkTaskState).Count();
+            }
+        }
+        public Int32 CompletedTaskCount
+        {
+            get
+            {
+                return Tasks.Where(t => t.State == CompletedTaskState).Count();
+            }
+        }
+
+        public Int32 EmployeeCount
+        {
+            get
+            {
+                return ProjectWorks.Count;
+            }
+        } 
+    }
+}
---
+++ VirtuumLab/VL.SUP/Views/Pages/ProjectsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using VLSUP.Repository;
+
+namespace VLSUP.Views
+{
+    /// <summary>
+    /// Interaction logic for ProjectsPage.xaml
+    /// </summary>
+    public partial class ProjectsPage : Page
+    {
+        private readonly SUPEntities db = new SUPEntities();
+        public ProjectsPage()
+        {
+            InitializeComponent();
+            Project[] projects = db.Projects.Where(p => !p.IsRemoved).ToArray();
+            projectsBox.ItemsSource = projects;
+        }
+    }
+}
